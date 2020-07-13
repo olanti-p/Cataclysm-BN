b8ee95eb532f832deb2f1894f38a9deff8b1e760
KorGgenT <curtis.r.merrill@gmail.com>
2020-07-13 08:12:54 -0400

    enchant_vals::mod::THIRST

--- src/character.cpp
+++ src/character.cpp
@@ -4767,6 +4767,8 @@ needs_rates Character::calc_needs_rates() const
         rates.thirst *= 0.25f;
     }
 
+    rates.thirst = enchantment_cache.modify_value( enchant_vals::mod::THIRST, rates.thirst );
+
     return rates;
 }
 
