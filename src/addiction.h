c42378eef8eb0f84bd5d13633912839b9669dfac
olanti-p <olanti-p@yandex.ru>
2021-03-22 12:42:12 +0300

    Remove misc headers

--- src/addiction.h
+++ src/addiction.h
@@ -4,10 +4,11 @@
 
 #include <string>
 
-#include "pldata.h"
 #include "type_id.h"
 
+class addiction;
 class Character;
+enum class add_type : int;
 
 // Minimum intensity before effects are seen
 constexpr int MIN_ADDICTION_LEVEL = 3;
