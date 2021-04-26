2a14c1dd0a4b1950890a0057434d08d99892f205
olanti-p <olanti-p@yandex.ru>
2021-04-26 12:25:09 +0300

    [temp] Test all gettext plural expressions

--- tests/cata_libintl_test.cpp
+++ tests/cata_libintl_test.cpp
@@ -496,3 +496,62 @@ TEST_CASE( "load_all_base_game_mos", "[libintl][i18n]" )
         }
     }
 }
+
+struct plural_table_entry {
+    const char *lang;
+    const char *language;
+    const char *value;
+};
+
+struct plural_table_entry plural_table[] = {
+    { "ja", "Japanese",          "nplurals=1; plural=0;" },
+    { "vi", "Vietnamese",        "nplurals=1; plural=0;" },
+    { "ko", "Korean",            "nplurals=1; plural=0;" },
+    { "en", "English",           "nplurals=2; plural=(n != 1);" },
+    { "de", "German",            "nplurals=2; plural=(n != 1);" },
+    { "nl", "Dutch",             "nplurals=2; plural=(n != 1);" },
+    { "sv", "Swedish",           "nplurals=2; plural=(n != 1);" },
+    { "da", "Danish",            "nplurals=2; plural=(n != 1);" },
+    { "no", "Norwegian",         "nplurals=2; plural=(n != 1);" },
+    { "nb", "Norwegian Bokmal",  "nplurals=2; plural=(n != 1);" },
+    { "nn", "Norwegian Nynorsk", "nplurals=2; plural=(n != 1);" },
+    { "fo", "Faroese",           "nplurals=2; plural=(n != 1);" },
+    { "es", "Spanish",           "nplurals=2; plural=(n != 1);" },
+    { "pt", "Portuguese",        "nplurals=2; plural=(n != 1);" },
+    { "it", "Italian",           "nplurals=2; plural=(n != 1);" },
+    { "bg", "Bulgarian",         "nplurals=2; plural=(n != 1);" },
+    { "el", "Greek",             "nplurals=2; plural=(n != 1);" },
+    { "fi", "Finnish",           "nplurals=2; plural=(n != 1);" },
+    { "et", "Estonian",          "nplurals=2; plural=(n != 1);" },
+    { "he", "Hebrew",            "nplurals=2; plural=(n != 1);" },
+    { "eo", "Esperanto",         "nplurals=2; plural=(n != 1);" },
+    { "hu", "Hungarian",         "nplurals=2; plural=(n != 1);" },
+    { "tr", "Turkish",           "nplurals=2; plural=(n != 1);" },
+    { "pt_BR", "Brazilian",      "nplurals=2; plural=(n > 1);" },
+    { "fr", "French",            "nplurals=2; plural=(n > 1);" },
+    { "lv", "Latvian",           "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n != 0 ? 1 : 2);" },
+    { "ga", "Irish",             "nplurals=3; plural=n==1 ? 0 : n==2 ? 1 : 2;" },
+    { "ro", "Romanian",          "nplurals=3; plural=n==1 ? 0 : (n==0 || (n%100 > 0 && n%100 < 20)) ? 1 : 2;" },
+    { "lt", "Lithuanian",        "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && (n%100<10 || n%100>=20) ? 1 : 2);" },
+    { "ru", "Russian",           "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);" },
+    { "uk", "Ukrainian",         "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);" },
+    { "be", "Belarusian",        "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);" },
+    { "sr", "Serbian",           "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);" },
+    { "hr", "Croatian",          "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);" },
+    { "cs", "Czech",             "nplurals=3; plural=(n==1) ? 0 : (n>=2 && n<=4) ? 1 : 2;" },
+    { "sk", "Slovak",            "nplurals=3; plural=(n==1) ? 0 : (n>=2 && n<=4) ? 1 : 2;" },
+    { "pl", "Polish",            "nplurals=3; plural=(n==1 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);" },
+    { "sl", "Slovenian",         "nplurals=4; plural=(n%100==1 ? 0 : n%100==2 ? 1 : n%100==3 || n%100==4 ? 2 : 3);" }
+};
+
+TEST_CASE( "gnu_gettext_plurals", "[libintl][i18n]" )
+{
+    for( const plural_table_entry &it : plural_table ) {
+        CAPTURE( it.lang );
+        std::string input = it.value;
+        input.pop_back();
+        input = input.substr( 19 );
+        cata_internal::PlfNodePtr ptr = cata_internal::parse_plural_rules( input );
+        REQUIRE( ptr );
+    }
+}
