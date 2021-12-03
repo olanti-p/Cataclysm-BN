917aad435daa243939a9e18f6bb3cd690f939810
Olanti <olanti-p@yandex.ru>
2021-12-03 12:28:31 +0300

    Fix (?) gcc 8 build

--- tests/dependency_tree_test.cpp
+++ tests/dependency_tree_test.cpp
@@ -6,8 +6,11 @@
 
 using t_mod_id = const char*;
 using t_mod_list = std::vector<t_mod_id>;
-using t_key_dep_map = std::map<t_mod_id, t_mod_list>;
-using t_map_entry = std::pair<t_mod_id, t_mod_list>;
+struct t_map_entry {
+    t_mod_id id;
+    t_mod_list list;
+};
+using t_key_dep_map = std::vector<t_map_entry>;
 
 static std::map<mod_id, std::vector<mod_id>> build_map( const t_key_dep_map &m )
 {
@@ -15,10 +18,10 @@ static std::map<mod_id, std::vector<mod_id>> build_map( const t_key_dep_map &m )
 
     for( const t_map_entry &entry : m ) {
         std::vector<mod_id> ids;
-        for( t_mod_id elem : entry.second ) {
+        for( t_mod_id elem : entry.list ) {
             ids.emplace_back( elem );
         }
-        ret.emplace( entry.first, ids );
+        ret.emplace( entry.id, ids );
     }
 
     return ret;
