3256fd9ecbb6b8b0122ff66ff14c73932b1b2f4e
John Bytheway <jbytheway@gmail.com>
2021-12-04 15:34:07 +0300

    Add units::angle
    Add a typesafe angle type.  Partly for general type safety but also to
    avoid confusion between different measures of angle (radians, degrees,
    and arcmin are all used in this codebase.
    
    (cherry picked from commit 2c04d796ef877401053439d9ffecfcaa96bc5b2e)

--- src/units_def.h
+++ src/units_def.h
@@ -126,6 +126,18 @@ class quantity
         value_type value_;
 };
 
+template<typename V, typename U>
+inline quantity<V, U> fabs( quantity<V, U> q )
+{
+    return quantity<V, U>( std::fabs( q.value() ), U{} );
+}
+
+template<typename V, typename U>
+inline quantity<V, U> fmod( quantity<V, U> num, quantity<V, U> den )
+{
+    return quantity<V, U>( std::fmod( num.value(), den.value() ), U{} );
+}
+
 /**
  * Multiplication and division with scalars. Result is a quantity with the same unit
  * as the input.
