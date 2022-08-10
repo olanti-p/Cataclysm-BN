aa8feb04be2ed38c70505baef913718c7c5f8d26
Olanti <olanti-p@yandex.ru>
2022-08-10 23:04:27 +0300

    Fix function visibility

--- src/overmap.h
+++ src/overmap.h
@@ -446,10 +446,12 @@ class overmap
             const overmap_connection &connection, const pf::directed_path<point_om_omt> &path, int z,
             const om_direction::type &initial_dir = om_direction::type::invalid,
             const om_direction::type &final_dir = om_direction::type::invalid );
+    public:
         void build_connection( const point_om_omt &source, const point_om_omt &dest, int z,
                                const overmap_connection &connection, bool must_be_unexplored,
                                const om_direction::type &initial_dir = om_direction::type::invalid,
                                const om_direction::type &final_dir = om_direction::type::invalid );
+    private:
         void connect_closest_points( const std::vector<point_om_omt> &points, int z,
                                      const overmap_connection &connection );
         // Polishing
