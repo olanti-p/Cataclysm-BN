c374ccd7b27c2f7944aafad0a0de48e7b68f6149
olanti-p <olanti-p@users.noreply.github.com>
2022-10-10 22:35:15 +0000

    deploy: 22ef16d4a842cfbb8cdc4a3056be4a254030d355

---
+++ lightmap_8cpp.js
@@ -0,0 +1,22 @@
+var lightmap_8cpp =
+[
+    [ "cast_zlight", "lightmap_8cpp.html#ac46962badafdae5dd4f37158c19433ae", null ],
+    [ "cast_zlight< float, shrapnel_calc, shrapnel_check, accumulate_fragment_cloud >", "lightmap_8cpp.html#a52c749d1ba525f4cf88395ed15e8927a", null ],
+    [ "cast_zlight< float, sight_calc, sight_check, accumulate_transparency >", "lightmap_8cpp.html#a668a9da7b042021d0d581089eb129073", null ],
+    [ "cast_zlight_segment", "lightmap_8cpp.html#af2b0551a8b813bdadad4acef34acd4ee", null ],
+    [ "castLight", "lightmap_8cpp.html#a10618ecda5342bdb62bd0a46c3fd7f5c", null ],
+    [ "castLightAll", "lightmap_8cpp.html#a6cece234bf1be86cda7ad6b2ceef227f", null ],
+    [ "castLightAll< float, float, shrapnel_calc, shrapnel_check, update_fragment_cloud, accumulate_fragment_cloud >", "lightmap_8cpp.html#a6a570db7ca232f96ccd0b2d89dc82302", null ],
+    [ "castLightAll< float, four_quadrants, sight_calc, sight_check, update_light_quadrants, accumulate_transparency >", "lightmap_8cpp.html#a0432c61090555de0f59d2c613f5d55b2", null ],
+    [ "fastexp", "lightmap_8cpp.html#a65676c440c79257194dbc539ebe7ff6b", null ],
+    [ "light_calc", "lightmap_8cpp.html#a6e659e992e3d17811d760fee18718535", null ],
+    [ "light_check", "lightmap_8cpp.html#a612231c2a1b076e6692b9da85cc27b2a", null ],
+    [ "lightmap_boundary_max", "lightmap_8cpp.html#ace2e71f6c24ff8f4212e34cdd81a6965", null ],
+    [ "quadrant_from_x_y", "lightmap_8cpp.html#a23445e2078ba8767b2aae74fe974d16d", null ],
+    [ "effect_haslight", "lightmap_8cpp.html#a3d11805905cbb2b08ce256c3421ecae3", null ],
+    [ "effect_onfire", "lightmap_8cpp.html#afcfa82c36c0dc2d76cd19d622c2b6ab4", null ],
+    [ "lightmap_boundaries", "lightmap_8cpp.html#a72fc3686b8539164ec5b39114a093f8a", null ],
+    [ "lightmap_boundary_min", "lightmap_8cpp.html#ac743592d8b3668630916bd90701d7e0a", null ],
+    [ "LIGHTMAP_CACHE_X", "lightmap_8cpp.html#af54aece6c3d081e409263f5a60006f38", null ],
+    [ "LIGHTMAP_CACHE_Y", "lightmap_8cpp.html#ac119d6f9e5feca445d86e727c8aa9d73", null ]
+];
