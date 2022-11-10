13fd68055aef992845ec18824a1bea645c2b3024
Olanti <olanti-p@yandex.ru>
2022-11-11 00:33:20 +0300

    Fix astyle

--- src/sdltiles.cpp
+++ src/sdltiles.cpp
@@ -3405,7 +3405,7 @@ static void CheckMessages()
         // on focus gain. This seems to mess up the first redraw and
         // causes black screen that lasts ~0.5 seconds before the screen
         // contents are redrawn in the following code.
-        if ( editor::show_cata_ui() ) {
+        if( editor::show_cata_ui() ) {
             ui_manager::invalidate( rectangle<point>( point_zero, point( WindowWidth, WindowHeight ) ), false );
             ui_manager::redraw_invalidated();
         }
