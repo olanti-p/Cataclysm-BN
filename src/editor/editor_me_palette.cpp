9e04ffedd5a1d3dcc6b98321a3533ea877f0623d
Olanti <olanti-p@yandex.ru>
2022-11-14 02:52:30 +0300

    Switch palette to new vector widget

--- src/editor/editor_me_palette.cpp
+++ src/editor/editor_me_palette.cpp
@@ -29,63 +29,7 @@ void show_palette_entry_extended( me_state &state, editor::me_palette &p,
 
     ImGui::Text( "Pieces:" );
 
-    cata::optional<size_t> del;
-    cata::optional<size_t> move_up;
-    cata::optional<size_t> move_dn;
     auto &list = entry.mapping.pieces;
-    for( size_t i = 0; i < list.size(); i++ ) {
-        ImGui::PushID( i );
-        ImGui::Separator();
-
-        if( ImGui::ImageButton( "del", "me_delete" ) ) {
-            del = i;
-        }
-        ImGui::HelpPopup( "Delete piece." );
-        ImGui::SameLine();
-
-        if( i == 0 ) {
-            ImGui::BeginDisabled();
-        }
-        if( ImGui::ArrowButton( "up", ImGuiDir_Up ) ) {
-            move_up = i;
-        }
-        ImGui::HelpPopup( "Move piece up." );
-        if( i == 0 ) {
-            ImGui::EndDisabled();
-        }
-        ImGui::SameLine();
-
-        if( i == list.size() - 1 ) {
-            ImGui::BeginDisabled();
-        }
-        if( ImGui::ArrowButton( "down", ImGuiDir_Down ) ) {
-            move_dn = i;
-        }
-        ImGui::HelpPopup( "Move piece down." );
-        if( i == list.size() - 1 ) {
-            ImGui::EndDisabled();
-        }
-        ImGui::SameLine();
-
-        ImGui::Text( "Piece %d: %s", static_cast<int>( i ),
-                     io::enum_to_string<PieceType>( list[i]->get_type() ).c_str() );
-
-        list[i]->show_ui( state );
-
-        ImGui::PopID();
-    }
-    if( del ) {
-        list.erase( list.begin() + *del );
-        state.mark_changed();
-    }
-    if( move_up ) {
-        std::swap( list[*move_up], list[*move_up - 1] );
-        state.mark_changed();
-    }
-    if( move_dn ) {
-        std::swap( list[*move_dn], list[*move_dn + 1] );
-        state.mark_changed();
-    }
 
     static std::string new_piece_str;
     static std::vector<PieceType> piece_opts;
@@ -99,13 +43,34 @@ void show_palette_entry_extended( me_state &state, editor::me_palette &p,
         }
     }
 
-    int new_piece_type = 0;
-    ImGui::Separator();
-    if( ImGui::Combo( "##pick-new-piece", &new_piece_type, new_piece_str.c_str() ) ) {
-        if( new_piece_type != 0 ) {
-            list.push_back( editor::make_new_piece( piece_opts[new_piece_type - 1] ) );
-            state.mark_changed();
+    bool changed = ImGui::VectorWidget()
+    .with_add( [&]()->bool {
+        bool ret = false;
+        ImGui::Separator();
+        int new_piece_type = 0;
+        if( ImGui::Combo( "##pick-new-piece", &new_piece_type, new_piece_str.c_str() ) )
+        {
+            if( new_piece_type != 0 ) {
+                list.push_back( editor::make_new_piece( piece_opts[new_piece_type - 1] ) );
+                ret = true;
+            }
         }
+        return ret;
+    } )
+    .with_for_each( [&]( size_t idx ) {
+        ImGui::Separator();
+        ImGui::Text( "Piece %d: %s", static_cast<int>( idx ),
+                     io::enum_to_string<PieceType>( list[idx]->get_type() ).c_str() );
+
+        list[idx]->show_ui( state );
+    } )
+    .with_duplicate( [&]( size_t idx ) {
+        list.insert( std::next( list.cbegin(), idx + 1 ), list[idx]->clone() );
+    } )
+    .run( list );
+
+    if( changed ) {
+        state.mark_changed();
     }
 
     ImGui::End();
@@ -127,118 +92,29 @@ static void show_palette_entries( me_state &state, std::vector<me_palette_entry>
         }
     }
 
-    cata::optional<size_t> del;
-    cata::optional<size_t> dupe;
-    cata::optional<size_t> move_up;
-    cata::optional<size_t> move_dn;
-    for( size_t i = 0; i < list.size(); i++ ) {
-        ImGui::PushID( i );
-        if( ImGui::ImageButton( "del", "me_delete" ) ) {
-            del = i;
+    bool changed = ImGui::VectorWidget()
+    .with_add( [&]() -> bool {
+        bool ret = false;
+        if( ImGui::ImageButton( "add", "me_add" ) )
+        {
+            list.emplace_back( me_palette_entry{
+                state.file().uuid_gen(),
+                state.file().base.pick_available_key(),
+                col_default_piece_color,
+                false,
+                cata::nullopt,
+                ter_eid::NULL_ID(),
+                furn_eid::NULL_ID(),
+                me_mapping()
+            } );
+            ret = true;
         }
-        ImGui::HelpPopup( "Delete entry." );
-        ImGui::SameLine();
-
-        if( ImGui::ImageButton( "dupe", "me_duplicate" ) ) {
-            dupe = i;
-        }
-        ImGui::HelpPopup( "Duplicate entry." );
-        ImGui::SameLine();
-
-        if( i == 0 ) {
-            ImGui::BeginDisabled();
-        }
-        if( ImGui::ArrowButton( "up", ImGuiDir_Up ) ) {
-            move_up = i;
-        }
-        ImGui::HelpPopup( "Move entry up." );
-        if( i == 0 ) {
-            ImGui::EndDisabled();
-        }
-        ImGui::SameLine();
-
-        if( i == list.size() - 1 ) {
-            ImGui::BeginDisabled();
-        }
-        if( ImGui::ArrowButton( "down", ImGuiDir_Down ) ) {
-            move_dn = i;
-        }
-        ImGui::HelpPopup( "Move entry down." );
-        if( i == list.size() - 1 ) {
-            ImGui::EndDisabled();
-        }
-        ImGui::SameLine();
-
-        uuid_t &brush = state.tools_state->brush;
-        if( list[i].uuid == brush ) {
-            if( ImGui::ImageButton( "unpick", "me_clear_rows_brush" ) ) {
-                brush = UUID_INVALID;
-            }
-            ImGui::HelpPopup( "Unselect (turns brush into eraser)." );
-        } else {
-            if( ImGui::ImageButton( "pick", "me_set_rows_brush" ) ) {
-                brush = list[i].uuid;
-            }
-            ImGui::HelpPopup( "Select as active for brush." );
-        }
-        ImGui::SameLine();
-
-        if( ImGui::ColorEdit4( "MyColor##3", ( float * )&list[i].color,
-                               ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_NoLabel ) ) {
-            state.mark_changed( "palette-entry-color" );
-        }
-        ImGui::SameLine();
-
-        ImGui::SetNextItemWidth( ImGui::GetFrameHeight() );
-        bool is_dupe_symbol = dupe_symbols.count( list[i].key ) > 0;
-        if( is_dupe_symbol ) {
-            ImGui::BeginErrorArea();
-        }
-        if( ImGui::InputSymbol( "##key", list[i].key.str, default_map_key.str.c_str() ) ) {
-            state.mark_changed( "palette-entry-key" );
-        }
-        ImGui::HelpPopup( "Symbol to use on canvas." );
-        if( is_dupe_symbol ) {
-            ImGui::EndErrorArea();
-        }
-        ImGui::SameLine();
-        ImGui::SetNextItemWidth( ImGui::GetFrameHeight() * 15.0f );
-        if( ImGui::InputId( "##furn", list[i].furn ) ) {
-            state.mark_changed();
-            list[i].sprite_cache_valid = false;
-        }
-        ImGui::SameLine();
-        ImGui::SetNextItemWidth( ImGui::GetFrameHeight() * 15.0f );
-        if( ImGui::InputId( "##ter", list[i].ter ) ) {
-            state.mark_changed();
-            list[i].sprite_cache_valid = false;
-        }
-        ImGui::SameLine();
-        if( ImGui::ArrowButton( "##mapping", ImGuiDir_Right ) ) {
-            state.uistate->view_mappings = list[i].uuid;
-        }
-        ImGui::PopID();
-    }
-    if( del ) {
-        const uuid_t &uuid = list[ *del ].uuid;
-        state.file().base.remove_usages( uuid );
-        if( state.tools_state->brush == uuid ) {
-            state.tools_state->brush = UUID_INVALID;
-        }
-        list.erase( list.begin() + *del );
-        state.mark_changed();
-    }
-    if( move_up ) {
-        std::swap( list[*move_up], list[*move_up - 1] );
-        state.mark_changed();
-    }
-    if( move_dn ) {
-        std::swap( list[*move_dn], list[*move_dn + 1] );
-        state.mark_changed();
-    }
-    if( dupe ) {
-        const me_palette_entry &src = list[ *dupe ];
-        list.insert( std::next( list.cbegin(), *dupe + 1 ), me_palette_entry{
+        ImGui::HelpPopup( "Add new entry." );
+        return ret;
+    } )
+    .with_duplicate( [&]( size_t idx ) {
+        const me_palette_entry &src = list[ idx ];
+        list.insert( std::next( list.cbegin(), idx + 1 ), me_palette_entry{
             state.file().uuid_gen(),
             state.file().base.pick_available_key(),
             src.color,
@@ -248,22 +124,70 @@ static void show_palette_entries( me_state &state, std::vector<me_palette_entry>
             src.furn,
             src.mapping
         } );
+    } )
+    .with_delete( [&]( size_t idx ) {
+        const uuid_t &uuid = list[ idx ].uuid;
+        state.file().base.remove_usages( uuid );
+        if( state.tools_state->brush == uuid ) {
+            state.tools_state->brush = UUID_INVALID;
+        }
+        list.erase( std::next( list.cbegin(), idx ) );
+    } )
+    .with_for_each( [&]( size_t idx ) {
+        uuid_t &brush = state.tools_state->brush;
+        if( list[idx].uuid == brush ) {
+            if( ImGui::ImageButton( "unpick", "me_clear_rows_brush" ) ) {
+                brush = UUID_INVALID;
+            }
+            ImGui::HelpPopup( "Unselect (turns brush into eraser)." );
+        } else {
+            if( ImGui::ImageButton( "pick", "me_set_rows_brush" ) ) {
+                brush = list[idx].uuid;
+            }
+            ImGui::HelpPopup( "Select as active for brush." );
+        }
+        ImGui::SameLine();
+
+        if( ImGui::ColorEdit4( "MyColor##3", ( float * )&list[idx].color,
+                               ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_NoLabel ) ) {
+            state.mark_changed( "palette-entry-color" );
+        }
+        ImGui::SameLine();
+
+        ImGui::SetNextItemWidth( ImGui::GetFrameHeight() );
+        bool is_dupe_symbol = dupe_symbols.count( list[idx].key ) > 0;
+        if( is_dupe_symbol ) {
+            ImGui::BeginErrorArea();
+        }
+        if( ImGui::InputSymbol( "##key", list[idx].key.str, default_map_key.str.c_str() ) ) {
+            state.mark_changed( "palette-entry-key" );
+        }
+        ImGui::HelpPopup( "Symbol to use on canvas." );
+        if( is_dupe_symbol ) {
+            ImGui::EndErrorArea();
+        }
+        ImGui::SameLine();
+        ImGui::SetNextItemWidth( ImGui::GetFrameHeight() * 15.0f );
+        if( ImGui::InputId( "##furn", list[idx].furn ) ) {
+            state.mark_changed();
+            list[idx].sprite_cache_valid = false;
+        }
+        ImGui::SameLine();
+        ImGui::SetNextItemWidth( ImGui::GetFrameHeight() * 15.0f );
+        if( ImGui::InputId( "##ter", list[idx].ter ) ) {
+            state.mark_changed();
+            list[idx].sprite_cache_valid = false;
+        }
+        ImGui::SameLine();
+        if( ImGui::ArrowButton( "##mapping", ImGuiDir_Right ) ) {
+            state.uistate->view_mappings = list[idx].uuid;
+        }
+    } )
+    .run( list );
+
+    if( changed ) {
         state.mark_changed();
     }
-    if( ImGui::ImageButton( "add", "me_add" ) ) {
-        list.emplace_back( me_palette_entry{
-            state.file().uuid_gen(),
-            state.file().base.pick_available_key(),
-            col_default_piece_color,
-            false,
-            cata::nullopt,
-            ter_eid::NULL_ID(),
-            furn_eid::NULL_ID(),
-            me_mapping()
-        } );
-        state.mark_changed();
-    }
-    ImGui::HelpPopup( "Add new entry." );
 }
 
 void show_palette( me_state &state, me_palette &p, bool &show )
