e676b3adf17a5971ac3c1ec31951d4593713f31c
Olanti <olanti-p@yandex.ru>
2022-11-29 01:41:06 +0300

    Update build.sh

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -54,6 +54,10 @@ then
     if [ "$CATA_CLANG_TIDY" = "plugin" ]
     then
         cmake_extra_opts+=("-DCATA_CLANG_TIDY_PLUGIN=ON")
+        # Need to specify the particular LLVM / Clang versions to use, lest it
+        # use the llvm-7 that comes by default on the Travis Xenial image.
+        cmake_extra_opts+=("-DLLVM_DIR=/usr/lib/llvm-8/lib/cmake/llvm")
+        cmake_extra_opts+=("-DClang_DIR=/usr/lib/llvm-8/lib/cmake/clang")
     fi
 
     if echo "$COMPILER" | grep -q "clang"
