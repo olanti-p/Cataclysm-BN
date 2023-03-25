49f89447c02b20b5677fd500f734f6e0958458d9
Olanti <olanti-p@yandex.ru>
2023-03-25 12:28:38 +0300

    Update tools/clang-tidy-plugin/DeterminismCheck.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- tools/clang-tidy-plugin/DeterminismCheck.cpp
+++ tools/clang-tidy-plugin/DeterminismCheck.cpp
@@ -22,7 +22,7 @@ void DeterminismCheck::registerMatchers( MatchFinder *Finder )
     Finder->addMatcher(
         callExpr(
             callee(
-                  functionDecl(
+                functionDecl(
                     anyOf( hasName( "::rand" ), hasName( "::srand" ), hasName( "::random" ),
                            hasName( "::srandom" ), hasName( "::drand48" ),
                            hasName( "::erand48" ), hasName( "::lrand48" ), hasName( "::mrand48" ),
