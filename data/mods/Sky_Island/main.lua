c844ffe0aff3255b0febe44781e28754aa4e6eec
Olanti <olanti-p@yandex.ru>
2023-08-29 01:35:09 +0300

    Refactor

--- data/mods/Sky_Island/main.lua
+++ data/mods/Sky_Island/main.lua
@@ -8,6 +8,27 @@ assert( mod.island_overspecial_id:is_valid() )
 -- Somewhat counter-intuitive, but the island is designed relative to ground level
 mod.island_spawn_zlev = 0
 
+-- Place overmap special somewhere within the overmap at specified z level.
+-- Returns 2 values: (bool, Tripoint)
+-- (Whether force placement took place, and at what position oms was placed)
+mod.place_oms_in_overmap = function( overmap, oms, num_attempts, zlev )
+    local om_size = const.OM_OMT_SIZE
+    -- Let's try N times
+    for attempt = 0,num_attempts do
+        local p = Tripoint.new( gapi.rng(0, om_size), gapi.rng(0, om_size), zlev )
+        if overmap:can_place_special( oms, p, OvermapDir.north, false ) then
+            local city = overmap:get_nearest_city( p )
+            overmap:place_special( oms, p, OvermapDir.north, city, false, false )
+            return false, p
+        end
+    end
+    -- Tough luck. Let's just plop it right in the center.
+    local p = Tripoint.new(om_size // 2, om_size // 2, zlev)
+    local city = overmap:get_nearest_city( p )
+    overmap:place_special( oms, p, OvermapDir.north, city, false, true )
+    return true, p
+end
+
 mod.place_sky_island = function( overmap, p_om )
     --[[
         When looking for starting location, the game scans 3 overmaps in each 
@@ -21,22 +42,14 @@ mod.place_sky_island = function( overmap, p_om )
     ]]
     local p_desired_om = Point.new( 1, 1 )
     if p_om == p_desired_om then
-        local om_size = const.OM_OMT_SIZE
         local oms = mod.island_overspecial_id:obj()
-        -- Let's try 100 times
-        for attempt = 0,100 do
-            local p = Tripoint.new( gapi.rng(0, om_size), gapi.rng(0, om_size), mod.island_spawn_zlev )
-            if overmap:can_place_special( oms, p, OvermapDir.north, false ) then
-                local city = overmap:get_nearest_city( p )
-                overmap:place_special( oms, p, OvermapDir.north, city, false, false )
-                gdebug.log_info("Placed Sky Island at "..tostring(p))
-                return
-            end
+        local num_attempts = 0
+        local forced, p = mod.place_oms_in_overmap( overmap, oms, num_attempts, mod.island_spawn_zlev )
+
+        local msg = "Placed Sky Island at "..tostring(p)
+        if forced then
+            msg = msg.." (forced)"
         end
-        -- Tough luck. How this happened is a mystery, let's just force our way through.
-        local p = Tripoint.new(om_size // 2, om_size // 2, mod.island_spawn_zlev)
-        local city = overmap:get_nearest_city( p )
-        overmap:place_special( oms, p, OvermapDir.north, city, false, true )
-        gdebug.log_info("Placed Sky Island at "..tostring(p).." (forced)")
+        gdebug.log_info(msg)
     end
 end
