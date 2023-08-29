5291378047a795840a33d2dd4c8cb52d99134930
Olanti <olanti-p@yandex.ru>
2023-08-29 19:26:58 +0300

    Add difficulty levels

--- data/mods/Sky_Island/main.lua
+++ data/mods/Sky_Island/main.lua
@@ -8,6 +8,39 @@ assert( mod.island_overspecial_id:is_valid() )
 -- Somewhat counter-intuitive, but the island is designed relative to ground level
 mod.island_spawn_zlev = 0
 
+mod.diff_casual = 1
+mod.diff_normal = 2
+mod.diff_hard = 3
+mod.diff_impossible = 4
+
+--[[
+    TODO: Since we're using locale.gettext here, and call it on game start,
+    if player changes the language mid-game the strings won't use the new language until restart.
+    If we had translation class we'd be using that here.
+]]
+mod.difficulty_data = {
+    [mod.diff_casual] = {
+        name = locale.gettext("Casual"),
+        descr = locale.gettext("Time limits are more relaxed.\nWarp pulses occur every 90 minutes.  This means warp sickness sets in after 12 hours of being earthside, and disintegration at 18 hours.\nYou will have more time for activities and reaching the exit should generally not be a problem."),
+        interval = TimeDuration.from_minutes( 90 )
+    },
+    [mod.diff_normal] = {
+        name = locale.gettext("Normal"),
+        descr = locale.gettext("This is the intended way to play.\nWarp pulses occur every 45 minutes.  This means on every expedition, you will have 6 hours to explore, fight, loot, and find the exit before warp sickness sets in, and 8 hours total before disintegration begins.\nGetting to the exit is not a cakewalk, but you should have time to explore while earthside."),
+        interval = TimeDuration.from_minutes( 45 )
+    },
+    [mod.diff_hard] = {
+        name = locale.gettext("Hard"),
+        descr = locale.gettext("Time limits are strict.\nWarp pulses occur every 30 minutes.  This means warp sickness sets in after only 4 hours of being earthside, and disintegration at 6 hours.\nYou will have much less time to spare and must make getting to the exit your immediate priority."),
+        interval = TimeDuration.from_minutes( 30 )
+    },
+    [mod.diff_impossible] = {
+        name = locale.gettext("Impossible"),
+        descr = locale.gettext("Time limits are extremely tight.\nWarp pulses occur every 15 minutes.  This means warp sickness sets in after only 2 hours of being earthside, and disintegration at 3 hours.\nReaching the exit alive will take all the time you can spare, and warp sickness will be common.  You will not be given mercy!"),
+        interval = TimeDuration.from_minutes( 15 )
+    },
+}
+
 -- Place overmap special somewhere within the overmap at specified z level.
 -- Returns 2 values: (bool, Tripoint)
 -- (Whether force placement took place, and at what position oms was placed)
