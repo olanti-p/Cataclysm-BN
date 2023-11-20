8315d74bf0251d964348177827d4a826fb92552e
Olanti <olanti-p@yandex.ru>
2023-11-20 14:22:11 +0300

    Refactor rect normalization

--- src/editor/common/math.h
+++ src/editor/common/math.h
@@ -29,4 +29,16 @@ inline int wrap_index( int idx, int size )
 
 std::vector<point> line_bresenham( point a, point b );
 
+/**
+ * Given 2 opposite rectangle corners, returns lower-left and upper-right corners (min coords & max coords)
+ */
+template<typename Point>
+std::pair<Point, Point> normalize_rect( Point a, Point b )
+{
+    return {
+        Point( std::min( a.x(), b.x() ), std::min( a.y(), b.y() ) ),
+        Point( std::max( a.x(), b.x() ), std::max( a.y(), b.y() ) )
+    };
+}
+
 #endif // CATA_SRC_EDITOR_MATH_H
